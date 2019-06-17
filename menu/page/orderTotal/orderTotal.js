fb5af51300903bb4c50d63ce19a5701d93e543dc
waveyan <593310640@qq.com>
2019-06-17 21:44:59 +0800

    refund

--- menu/page/orderTotal/orderTotal.js
+++ menu/page/orderTotal/orderTotal.js
@@ -1,14 +1,15 @@
 var util = require('../../util/util.js')
-var api=require('../../util/api.js')
+var api = require('../../util/api.js')
 Page({
   data: {
     navTab: ["进行中", "已完成"],
     currentNavtab: 0,
     ordersList: null,
-    list:null,
+    list: null,
+    addtellHidden: true, //弹出框显示/隐藏
   },
-  onShow:function(){
-    var that=this;
+  onShow: function() {
+    var that = this;
     that.getMyOrders();
     console.log(that.data.ordersList)
   },
@@ -24,30 +25,76 @@ Page({
     }
   },
   //获取我的订单
-  getMyOrders:function(){
-    var that=this;
+  getMyOrders: function() {
+    var that = this;
     wx.request({
-      url: api.apiPath+'/userapi/getMyOrders',
-      header:{'access-token':api.getAccessToken()},
-      data:{'page':1,'start':0,'limit':1000},
-      method:'POST',
-      success(res){
-        var data=res.data;
+      url: api.apiPath + '/userapi/getMyOrders',
+      header: {
+        'access-token': api.getAccessToken()
+      },
+      data: {
+        'page': 1,
+        'start': 0,
+        'limit': 1000
+      },
+      method: 'POST',
+      success(res) {
+        var data = res.data;
         console.log(data);
-        if(data.code==0){
-        that.setData({
-          list:data.data[that.data.currentNavtab],
-          ordersList:data.data,
-        });
+        if (data.code == 0) {
+          that.setData({
+            list: data.data[that.data.currentNavtab],
+            ordersList: data.data,
+          });
         }
       }
     })
 
   },
-  goToComment:function(e){
+  goToComment: function(e) {
     wx.navigateTo({
-      url: '../comment/comment?order=' + e.currentTarget.dataset.order+'&from=orderTotal',
+      url: '../comment/comment?order=' + e.currentTarget.dataset.order + '&from=orderTotal',
     })
-  }
+  },
+  footAddtell: function(e) {
+    //打开弹出框
+    console.log(e)
+    this.setData({
+      addtellHidden: false,
+      orderId:e.target.dataset.id,
+    })
+  },
+  modalConfirm: function() {
+    //弹出框确认操作
+    var that=this;
+    this.setData({
+      addtellHidden: true,
+    })
+    console.log(that.data);
+    wx.request({
+      url: api.apiPath+'/userapi/addRefundReason',
+      header:{'access-token':api.getAccessToken()},
+      data: { 'orderId': that.data.orderId, 'refund_reason': that.data.userTell},
+      method:'GET',
+      success(e){
+        console.log(e);
+        if(e.data.code==0){
+          that.onShow();
+        }
+      }
+    })
 
+  },
+  modalCancel: function() {
+    //弹出框取消操作
+    this.setData({
+      addtellHidden: true,
+    })
+  },
+  saveUsertell: function (e) {
+    var that=this;
+    that.setData({
+      userTell: e.detail.value,
+    });
+  }
 })
